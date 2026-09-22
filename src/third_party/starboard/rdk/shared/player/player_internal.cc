b7c950076b38ec05542d6ed8ddef93bf47fd08ca
sranan984 <Sourabh_Rananaware@comcast.com>
2026-09-22 12:42:21 +0000

    Remove unused name parameter from audio clipping element

--- src/third_party/starboard/rdk/shared/player/player_internal.cc
+++ src/third_party/starboard/rdk/shared/player/player_internal.cc
@@ -1994,7 +1994,7 @@ PlayerImpl::PlayerImpl(SbPlayer player,
   }
 
   if (audio_codec_ == kSbMediaAudioCodecPcm) {
-    GstElement* filter = elements::CreateAudioClippingElement(nullptr);
+    GstElement* filter = elements::CreateAudioClippingElement();
     g_object_set(pipeline_, "audio-filter", filter, nullptr);
   }
 
